dda40762d3b882be95481671e16e761057d5af32
Haniye <esmaeilihaniye@yahoo.com>
2023-11-25 12:51:10 -0800

    - Add animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private float _speed = 12f;
+    [SerializeField] private Animator _animator;
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        if (x == 0 && z == 0) 
+        {
+            _animator.SetBool("Walk", false); return; 
+        }
         Vector3 move = transform.right * x + transform.forward * z;
         _characterController.Move(move * _speed * Time.deltaTime);
+        _animator.SetBool("Walk", true);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,18 +4,28 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform _silencer;
     [SerializeField] private float _maxDistance = 100f;
+    [SerializeField] private Animator _animator;
     private void Update()
     {
         if (Input.GetButtonDown("Fire1")) 
         {
             Shoot();
         }
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            Reload();
+        }
     }
     private void Shoot() 
     {
-        if(Physics.Raycast(_silencer.position,transform.forward, _maxDistance)) 
-        {
-            Debug.Log("Hit");
-        }
+        _animator.SetTrigger("Fire");
+        //if (Physics.Raycast(_silencer.position,transform.forward, _maxDistance)) 
+        //{
+        //    Debug.Log("Hit");
+        //}
+    }
+    private void Reload() 
+    {
+        _animator.SetTrigger("Reload");
     }
 }
