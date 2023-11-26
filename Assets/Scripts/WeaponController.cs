3b1116ebec6734a55fcaf50fc05790246c15d5b6
Haniye <esmaeilihaniye@yahoo.com>
2023-11-26 04:16:09 -0800

    - Add RayCast Hit

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class WeaponController : MonoBehaviour
 {
     [SerializeField] private Transform _silencer;
     [SerializeField] private float _maxDistance = 100f;
     [SerializeField] private Animator _animator;
-    [SerializeField] private int _weaponSpace = 10;
+    [SerializeField] private int _magazine = 10;
     private int _numberOfBulletInWeapon;
     private void Start()
     {
-        _numberOfBulletInWeapon = _weaponSpace;
+        _numberOfBulletInWeapon = _magazine;
     }
     private void Update()
     {
@@ -27,14 +27,15 @@ public class Weapon : MonoBehaviour
         if (_numberOfBulletInWeapon == 0) return;
         _animator.SetTrigger("Fire");
         _numberOfBulletInWeapon --;
-        //if (Physics.Raycast(_silencer.position,transform.forward, _maxDistance)) 
-        //{
-        //    Debug.Log("Hit");
-        //}
+        RaycastHit hit;
+        if (Physics.Raycast(_silencer.position, transform.forward, out hit, _maxDistance))
+        {
+            hit.rigidbody.AddForce(new Vector3(10,10,500));
+        }
     }
     private void Reload() 
     {
-        _numberOfBulletInWeapon = _weaponSpace;
+        _numberOfBulletInWeapon = _magazine;
         _animator.SetTrigger("Reload");
     }
 }
