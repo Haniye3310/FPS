d391d39b2cc3f8bcfa0294209d153c981561bf6b
Haniye <esmaeilihaniye@yahoo.com>
2023-11-26 03:29:11 -0800

    - Add reload's logic

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,12 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Transform _silencer;
     [SerializeField] private float _maxDistance = 100f;
     [SerializeField] private Animator _animator;
+    [SerializeField] private int _weaponSpace = 10;
+    private int _numberOfBulletInWeapon;
+    private void Start()
+    {
+        _numberOfBulletInWeapon = _weaponSpace;
+    }
     private void Update()
     {
         if (Input.GetButtonDown("Fire1")) 
@@ -18,7 +24,9 @@ public class Weapon : MonoBehaviour
     }
     private void Shoot() 
     {
+        if (_numberOfBulletInWeapon == 0) return;
         _animator.SetTrigger("Fire");
+        _numberOfBulletInWeapon --;
         //if (Physics.Raycast(_silencer.position,transform.forward, _maxDistance)) 
         //{
         //    Debug.Log("Hit");
@@ -26,6 +34,7 @@ public class Weapon : MonoBehaviour
     }
     private void Reload() 
     {
+        _numberOfBulletInWeapon = _weaponSpace;
         _animator.SetTrigger("Reload");
     }
 }
